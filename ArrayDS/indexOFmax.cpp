4d368433f14d94b9c6be8f32462c204a388f6aaf
NEONITIN3 <nitingaud1999@gmail.com>
2022-12-29 06:51:16 +0530

    Max element index in array

--- ArrayDS/indexOFmax.cpp
+++ ArrayDS/indexOFmax.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
- 
-    
+    int a[]={1,23,4,21};
+    //brut force method 0(N)
+    int n=sizeof(a)/sizeof(a[0]); 
+    //cout<<n;
+    int max=0;
+    for(int i=0;i<n;i++){
+        if(a[i]>a[max])
+           max=i;
+    }
+     cout<<"index of max element -> "<<max <<" and elment is ->"<<a[max];
+   return 0;
+
 }
