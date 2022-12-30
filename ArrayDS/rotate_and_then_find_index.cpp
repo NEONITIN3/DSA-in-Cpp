8feb6e391326d5bcaea3c6a33805f937ae30115d
NEONITIN3 <nitingaud1999@gmail.com>
2022-12-30 22:47:36 +0530

    rotet and find that k index

--- ArrayDS/rotate_and_then_find_index.cpp
+++ ArrayDS/rotate_and_then_find_index.cpp
@@ -4,30 +4,19 @@ using namespace std;
 
     int search(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int>temp(n);
         for(int i=0;i<n;i++){
-            int result=(i+k)%n;
-             temp[result]=nums[i];
+            //(i+k)%n;  eASE MILEGA ROTED INDEX
              if (nums[i]==k)
-                 return result;
+                 return (i+k)%n;//ROTED INDEX PRINT KAR DENGE 
         }
         return -1;
-        // nums=temp;
-        // int s=0, last=n-1;
-        // int mid=(s+(n-s))/2;
-        // while(s<=last){
-        // if(nums[mid]==k)
-        //   return mid;
-        // else if(nums[mid]>k)
-        //   last=mid-1;
-        //   else
-        //     s=mid+1;
-        // }
-        // return -1;     
+       
     }
 
     int main(){
       vector<int>v={4,5,6,7,0,1,2};
-       int target = 4;
+       int target = 3; //esi 3 se rotat karna hai uske baad 3
+       // ko search bhi karo rotetd array m kaha hau uska index print karn ahia
+       // ROTED HONE K BAAD 0 1 2 4 5 6 7   //AB ESME TO 3 HAI NAHI TO -1 RETURN KAR DO
        cout<<search(v,target);
     }
