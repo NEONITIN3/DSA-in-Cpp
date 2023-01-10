d57328a32b6ec6cfb5821a791adc1946a3157621
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-10 22:46:28 +0530

    Height  BT with edge count

---
+++ TREE_DS/BT_height_with_edge_count.cpp
@@ -0,0 +1,52 @@
+//hight basically kya hota hai
+/*
+maximum lenght jo hogi leaf node se root tak ki wahi height hai
+to ham dfs laga k dekhenge left m koun max hai node phir right ka bhi dono m jo max hoga usme +1 kar denge 
+                                10
+                             20     30
+                         40    50
+                                  90
+                    //   height 4   
+                    //max path length from root to leaf m no. of nodeis the height                      
+*/
+
+//its also called HEIGHT
+
+//es case m ham maximum length m jo edge hoga usko count karemge for HEIGHT
+//us case se only -1 rahata hai
+#include<bits/stdc++.h>
+
+using namespace std;
+struct node{
+    int root;
+    node *left ,*right;
+    //cunstructor for node
+     node(int current){
+        root = current;
+        left=right=NULL;
+    }
+};
+int height_of_BT(node *current){
+    //base case 
+    if (current==NULL)
+    return -1;
+
+    //recursive function
+    //current->left se max aur right se max le k uska sum+1 se kar de to wahi max height hogi
+
+                  
+                 return  1+max(height_of_BT(current->left),height_of_BT(current->right));
+  
+
+
+}
+int main(){
+    node *root = new node(10);
+    root -> left = new node(20);
+    root -> right = new node(30);
+    root -> left -> left = new node(40);
+    // root -> left -> right = new node(50);
+    // root -> left -> right -> right = new node(90);
+    cout<<" height of this tree is  "<<height_of_BT(root);
+    return 0;
+}
