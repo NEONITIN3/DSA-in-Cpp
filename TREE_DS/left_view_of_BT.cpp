f4a3894c780dc38c7e5894507f4cc701f1a575cb
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-10 22:53:08 +0530

    left view  of BT

---
+++ TREE_DS/left_view_of_BT.cpp
@@ -0,0 +1,8 @@
+/*
+koi tree di hue haiuska left view print karna hai
+
+                        10
+                   20        30
+                40    50    45  80
+                left view 10 20 40 
+*/
