842e7cc427760b89d60af5cf526bdf7bffc44846
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-19 18:53:46 +0530

    bubble sort

---
+++ All-Sorting/bubbelSORT.cpp
@@ -0,0 +1,13 @@
+/*
+bubble sort m 
+swapping calti hai har pass m
+5 elemnt hai to 4 pass chalega har paas m do do elment array k compar e ho k swap honge 
+if a[i+1]<a[i] then swaping hogi
+jab n-1 pass chal jayega tab all element swap ho chuke honge
+but some time 
+kuchh pass remaining hote hai  tabhi sare element sorted ho chuke hote hai
+es cas eme hame baki k abche pass ko nahi chalana hai esko pata karne k liye ek flag lellenge
+agar kise bhi pass m koi swapping nahi hue to eska matlab sare elment sorte hai au rbaki k bache swapp ko 
+nahi chalana padega
+esase  timple complexity reduced ho jayegi
+*/
