3fd001d1b2bb9b07ef4da3212c0aa5377b616b9c
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-22 19:44:05 +0530

    prime number optimized solution

---
+++ c_programming/primeNumber_optimized.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    if(n==1 || n==0 || n%2==0 || n%3 ==0)
+    return false;
+    for(int i=5 ;i*i <= n;i=i+6)
+    if(n%i==0 || n%(i+2)==0){
+     return false;
+    }
+    return true;
+}
+int main(){
+    int n=100000098;
+    cout<<isprime(n);
+
+}
