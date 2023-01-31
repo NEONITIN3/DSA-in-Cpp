26dc34596cf3747dd89fab8f1c46a8ef6b8f3fdf
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-31 13:54:18 +0530

    seraching with two pointer

---
+++ SerachingDSA/TWO_pointer_searching.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    vector<int>v{1,23,2,17,11,10,4,6,5,87};
+    //search for 4 in this array
+    int left=0;
+    int right= v.size()-1;
+    cout<<"size of vector "<<right<<endl;
+    while(left<right){
+        if(v[left]==4)
+        {
+            cout<<"find by left pointer ,index of the element is  "<<left<<endl;
+        }
+        else if (v[right]==4)
+        {
+            cout<<"find by right pointer, index of the element is  "<<right<<endl;
+        }
+        left++;
+        right--;
+    }
+    return -1;
+}
