231d493fe83cb35abc40838bd1a071ee464a185e
NEONITIN3 <nitingaud1999@gmail.com>
2023-01-31 14:34:45 +0530

    XOR

--- BIT_OPRATOR(bit magic)/XOR.cpp
+++ BIT_OPRATOR(bit magic)/XOR.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+   // cout<<(5^5);  0
     int n=888888888;
     cout<<(n^(n-1));
+
     for(int i=2;i<=200;i++)
     cout<<i<<"^"<<(i-1)<<" = "<<(i^(i-1))<<endl;
-    int m=10;
+    int m=5;
     int c=0;
 
     //finding set bit in number
