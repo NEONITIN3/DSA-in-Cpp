396088da2b9ae5168b6c3d430b153aefc4c5e78a
NEONITIN3 <nitingaud1999@gmail.com>
2023-02-05 23:32:46 +0530

    string reverse

---
+++ STRING/hello.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"NEO";
+    return 0;
+}
--- STRING/reverseSTRING.cpp
+++ STRING/reverseSTRING.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 string reverseString(string str){
 //agar adha revrese kar de to apane aap purareverse ho jayega
 int j = str.length()-1;
@@ -8,12 +10,14 @@ for(int i = 0;i<str.length()/2; i++){
     char ch = str[i];
     str[i] = str[j];
     str[j] = ch;
+    j--;
  }
+
  return str;
 }
 
 int main(){
-    string s = "NEOnitin";
+    string s = "NEOnitin123@";
    // cin>>s;
     cout<<"reverse of ->  "<<s<<" "<<reverseString(s);
 }
